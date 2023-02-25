92a41ffe4cca759d673fa71fbc6dac83fac86910
Sam <2379093w@student.gla.ac.uk>
2023-02-25 16:42:57 +0000

    Created the encoder that successfully writes a binary string into the LSB of an image, encoder is not finished yet I still need to create the header

--- Encoder.java
+++ Encoder.java
@@ -1,8 +1,9 @@
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
-
 import javax.imageio.ImageIO;
+import java.lang.Math;
 
 public class Encoder {
 
@@ -43,6 +44,76 @@ public class Encoder {
         return payloadImg;
     }  
 
+    private static String replaceLastChar(String text, char newLastChar){
+        String removeLastChar = text.substring(0,text.length()-1);
+        return (removeLastChar+ newLastChar);
+    }
 
+
+    //Payload inserted into the image
+    private static BufferedImage insertPayload(String payload, BufferedImage photo){
+
+        int payloadPointer = 0;
+        int bitPackageCount = (int)Math.ceil(payload.length()/3);
+
+        int xPixelPointer = 0;
+        int yPixelPointer = 0;
+        
+
+        //The payload insertion is done in 3 bit packages for the RGB values,
+        // there is then the special case at the end for the last package that might not contain 3 bits of data
+        for(int packageIterator=0; packageIterator<=bitPackageCount; packageIterator++){
+
+            //Checks if the x pixel falls past the photo size
+            if(xPixelPointer>=photo.getWidth()){
+                xPixelPointer=0;
+                yPixelPointer++;
+            }
+
+            //Get the colour data for the current pixel
+            Color originalPixelColour = new Color(photo.getRGB(xPixelPointer, yPixelPointer));
+            int originalRedInt = originalPixelColour.getRed();
+            int originalGreenInt = originalPixelColour.getGreen();
+            int originalBlueInt = originalPixelColour.getBlue();
+
+            //Initialise the new colour data to the old colour data
+            int newRedInt = originalRedInt;
+            int newGreenInt = originalGreenInt;
+            int newBlueInt = originalBlueInt;           
+
+            //If statements are used to stop writing once payload is written
+            if(((payload.length()-1)-payloadPointer)>=0){
+
+                String redBinaryRep = Integer.toBinaryString(originalPixelColour.getRed());
+                    
+                redBinaryRep = replaceLastChar(redBinaryRep,payload.charAt(payloadPointer));
+                newRedInt =  Integer.parseInt(redBinaryRep, 2);
+                payloadPointer++;
+            }
+            if(((payload.length()-1)-payloadPointer)>=0){
+
+                String greenBinaryRep = Integer.toBinaryString(originalPixelColour.getGreen());
+                    
+                greenBinaryRep = replaceLastChar(greenBinaryRep,payload.charAt(payloadPointer));
+                newGreenInt =  Integer.parseInt(greenBinaryRep, 2);
+                payloadPointer++;
+            }
+            if(((payload.length()-1)-payloadPointer)>=0){
+
+                String blueBinaryRep = Integer.toBinaryString(originalPixelColour.getBlue());
+                    
+                blueBinaryRep = replaceLastChar(blueBinaryRep,payload.charAt(payloadPointer));
+                newBlueInt =  Integer.parseInt(blueBinaryRep, 2);
+                payloadPointer++;
+            }
+            
+            Color newColour = new Color(newRedInt, newBlueInt, newGreenInt);
+
+            photo.setRGB(xPixelPointer, yPixelPointer, newColour.getRGB());
+
+            xPixelPointer++;
+        }
+    return photo;
+    }
 
 } 
